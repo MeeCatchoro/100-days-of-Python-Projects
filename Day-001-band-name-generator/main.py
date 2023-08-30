c968cb3c9fab2f7c9cdef354adca495c46f681a2
MeeCatchoro <102626827+MeeCatchoro@users.noreply.github.com>
2023-08-30 13:22:19 -0300

    Day 1 done.

---
+++ Day-001-band-name-generator/main.py
@@ -0,0 +1,7 @@
+print("Bem vindo ao gerador de nomes de banda")
+
+cidade = input("Qual o nome da cidade em que você nasceu?\n")
+
+pet = input("Qual o nome do seu animal de estimação?\n")
+
+print("O nome da sua banda é: " + cidade + " "+ pet)
