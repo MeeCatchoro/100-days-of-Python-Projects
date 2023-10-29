448d45c00ae948dd3b8a71a5f09d97701d922c6a
MeeCatchoro <102626827+MeeCatchoro@users.noreply.github.com>
2023-10-29 16:49:53 -0300

    Day 19 start.

---
+++ Day-019-start/main.py
@@ -0,0 +1,38 @@
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# screen = Screen()
+#
+#
+# def move_forwards():
+#     tim.forward(10)
+#
+#
+# def move_backwards():
+#     tim.backward(10)
+#
+#
+# def turn_left():
+#     tim.left(5)
+#
+#
+# def turn_right():
+#     tim.right(5)
+#
+#
+# def clear_drawing():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
+#     tim.pendown()
+#
+#
+# screen.listen()
+#
+#
+# screen.onkey(fun=move_forwards, key="w")
+# screen.onkey(fun=move_backwards, key="s")
+# screen.onkey(fun=turn_right, key="d")
+# screen.onkey(fun=turn_left, key="a")
+# screen.onkey(fun=clear_drawing, key="c")
+# screen.exitonclick()
